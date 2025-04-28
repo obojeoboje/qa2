7ca5a98884cbb674dbd8b640b30dc97fa97c6375
alexanderpolynsky <alexanderpolynsky@gmail.com>
2025-04-28 14:20:50 +0300

    added video

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,26 +3,49 @@ import styles from './index.module.css';
 const About = () => {
   return (
     <div className={styles.about}>
-      <div className={styles.aboutTitle}>
-        <h2>Обо мне</h2>
-      </div>
-      <div className={styles.aboutRight}>
-        <h3>
-          Александр Полынских
-          <br /> Lead QA Engineer/Mentor
-        </h3>
-        <div className={styles.aboutText}>
-          <p>
-            Занимаюсь менторством полтора года, у меня за спиной десятки
-            трудоустроенных учеников. Мои выпускники работают в крупнейших компаниях РФ, продуктами которых ты наверняка пользуешься.
-          </p>
-          <p>
-            Регулярно хожу на собеседования, а также провожу их сам, поэтому
-            знаю актуальные требования к кандидату на рынке сейчас и не лью
-            воду, а даю ученикам только то, что им необходимо.
-          </p>
+      
+      {/* Контейнер для текста */}
+      <div className={styles.aboutContent}>
+        <div className={styles.aboutTitle}>
+          <h2>Обо мне</h2>
+        </div>
+
+        <div className={styles.aboutRight}>
+          <h3>
+            Александр Полынских
+            <br /> Lead QA Engineer/Mentor
+          </h3>
+          <div className={styles.aboutText}>
+            <p>
+              Занимаюсь менторством два года, у меня за спиной десятки
+              трудоустроенных учеников. Мои выпускники работают в крупнейших компаниях РФ, продуктами которых ты наверняка пользуешься.
+            </p>
+            <p>
+              Регулярно хожу на собеседования, а также провожу их сам, поэтому
+              знаю актуальные требования к кандидату на рынке сейчас и не лью
+              воду, а даю ученикам только то, что им необходимо.
+            </p>
+          </div>
         </div>
       </div>
+
+      {/* Новый отдельный блок для видео */}
+      <div className={styles.videoWrapper}>
+        <iframe
+          src="https://kinescope.io/embed/7yywKX7cPKnLkJ681hg4ui"
+          allow="autoplay; fullscreen; picture-in-picture; encrypted-media; gyroscope; accelerometer; clipboard-write; screen-wake-lock;"
+          frameBorder="0"
+          allowFullScreen
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+          }}
+        ></iframe>
+      </div>
+      
     </div>
   );
 };
